c842ea273a9b07678fbd0ad392a5d6dfe4421e9a
sumedh-shingade <44223852+sumedh-shingade@users.noreply.github.com>
2018-10-23 22:59:30 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+n=int(input())
+l=[]
+a=0
+b=1
+count=0
+if n<=0:
+   print("series does not exist")
+elif n==1:
+   print("0")
+elif n==2:
+   print('0,1')
+else:
+   l.append(0)
+   l.append(1)
+while count<n-2:
+   l.append(a+b)
+   c=a+b
+   b=c
+   count+=1
+print(l)
+
+	
+   
+   
