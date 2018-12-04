b40cf71a274ec6532b68200512c56ef36aa9db56
sumedh-shingade <44223852+sumedh-shingade@users.noreply.github.com>
2018-12-04 13:02:02 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,27 +2,29 @@
 #54
 #M
 
-
-n=int(input())
+n=int(input("Enter limit=>"))
 l=[]
 a=0
 b=1
 count=0
 if n<=0:
-   print("series does not exist")
+   print("series DNE")
 elif n==1:
-   print("0")
+     print("0")
 elif n==2:
-   print('0,1')
+     print("0,1")
 else:
-   l.append(0)
-   l.append(1)
-while count<n-2:
-   l.append(a+b)
-   c=a+b
-   b=c
-   count+=1
+     l.append(0)
+     l.append(1)
+     while count<n-2:
+           c=a+b
+           l.append(c)
+           a=b
+           b=c
+           count+=1
 print(l)
+
+
 
 	
    
